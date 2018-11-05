47063b209307d672c9feb74d5c012f4ecee3beaa
Tatjana Tiunova <t.tiunova@gmail.com>
2018-11-05 10:10:29 +0200

    initial commit

---
+++ api/src/main/scala/org/test/blacklist/api/App.scala
@@ -0,0 +1,14 @@
+package org.test.blacklist.api
+
+import akka.http.scaladsl.server.HttpApp
+
+object App extends HttpApp {
+
+  def main(args: Array[String]): Unit = {
+    startServer("0.0.0.0", port = 8080)
+  }
+
+  override def routes = pathSingleSlash {
+    complete("Hello Akka!")
+  }
+}
