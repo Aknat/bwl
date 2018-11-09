5d770b0218bb1c1b418a73357fb28a069f385585
Tatjana Tiunova <t.tiunova@gmail.com>
2018-11-09 15:13:55 +0200

    connection ti db

--- api/src/main/scala/org/test/bwl/api/App.scala
+++ api/src/main/scala/org/test/bwl/api/App.scala
@@ -1,11 +1,18 @@
 package org.test.bwl.api
 
 import akka.http.scaladsl.server.HttpApp
+import org.test.bwl.model.conf.DB
 
-object App extends HttpApp {
+object App extends HttpApp with DB {
 
   def main(args: Array[String]): Unit = {
-    startServer("0.0.0.0", port = 8080)
+
+    println(conf.getList("client.db.contact.points"))
+
+    println(session.getState.getConnectedHosts)
+
+
+    //startServer("0.0.0.0", port = 8080)
   }
 
   override def routes = pathSingleSlash {
